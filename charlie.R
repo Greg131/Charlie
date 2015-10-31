8fd2d70fb2eb2d2f6101cc0e0a0c286955c5f2b1
Grégoire ARGENTON <gregoire_argenton@yahoo.fr>
2015-10-31 13:27:29 +0100

    lecture fichiers

--- charlie.R
+++ charlie.R
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------
-# Datathon SNCF Simplon - Where is Charlie
+# Datathon SNCF Simplon - Sujet 4 : "Où est Charlie?"
 # ----------------------------------------------------------
 
 setwd("~/Dropbox/Datascience/Datathon Simplon.co x SNCF/Scripts")
@@ -8,14 +8,14 @@ getwd() # Get working directory
 if (!file.exists("../data"))       {
   dir.create("../data")
 }
+# Les fichiers de données sont supposés être dans ../data
+
+
 
 ? read.csv
 
 # Lecture des fichiers
-PI_TAG <- read.csv("../data/PI_TAG DCT.csv", sep = ";",check.names = FALSE)
-PI_TAG2 <- read.csv("../data/PI_TAG DCT.csv", sep = ";",header = FALSE, skip = 1)
-# pb dans les noms de variables à régler
-                   
+
 Fonctions_AS <- read.csv("../data/Fonctions_AS.csv", fileEncoding = "UTF-16LE",sep = "\t")
 
 Liste_AVP <- read.csv("../data/Liste_AVP.csv",fileEncoding = "UTF-16LE",sep = "\t")
@@ -34,11 +34,17 @@ Liste_Sous_Stations_SE <- read.csv("../data/Liste_Sous_Stations_SE.csv",fileEnco
 
 Liste_Zones_Cdv <- read.csv("../data/Liste_Zones_Cdv.csv",fileEncoding = "UTF-16LE",sep = "\t")
 
+PI_TAG <- read.csv("../data/PI_TAG DCT.csv", sep = ";",check.names = FALSE)
+#PI_TAG2 <- read.csv("../data/PI_TAG DCT.csv", sep = ";",header = FALSE, skip = 1)
+# pb dans les noms de variables à régler
+
 Referentiel_geomocalisation <- read.csv("../data/Referentiel geolocalisation.csv", sep = ";")
 
-Temperatures_PI <- read.csv("../data/Temperatures_PI.csv", sep = "\t",dec = ",")
+REX_SIG <- read.csv("../data/REX SIG.csv", sep = ";",check.names = FALSE)
 
-            )
+REX_Incidents <- read.csv("../data/REX_Incidents.csv", sep = ";",check.names = FALSE)
+
+Temperatures_PI <- read.csv("../data/Temperatures_PI.csv", sep = "\t",dec = ",")
 str(Temperatures_PI)
 summary(Temperatures_PI)
 Temperatures_PI
